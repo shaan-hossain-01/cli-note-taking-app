98bb7342a28c97932dc6bc258c0b67fed3202d25
Atiq Afzal Hossain Masud <rockshaan200@gmail.com>
2025-10-25 13:46:47 +0600

    started the project

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-#!/usr/bin/env node
-
-const note = process.argv[2]
-const newNote = {
-  content:note,
-  id: Date.now(),
-}
-console.log(newNote);
+import './src/command.js';
---
+++ src/command.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+const argv = yargs(hideBin(process.argv)).parse()
+
+if (argv.ships > 3 && argv.distance < 53.5) {
+  console.log('Plunder more riffiwobbles!')
+} else {
+  console.log('Retreat from the xupptumblers!')
+}
