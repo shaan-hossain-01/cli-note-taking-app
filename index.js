89afc71579fb34c5fdec1e31f4cb54ed82d22701
Atiq Afzal Hossain Masud <rockshaan200@gmail.com>
2025-10-25 14:43:20 +0600

    fix(cli): make bin robust to symlink by resolving index import path; add shebang to index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-import './src/command.js';
+#!/usr/bin/env node
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+await import(join(__dirname, "src", "command.js"));
